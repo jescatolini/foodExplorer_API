a0c54331f5fcf4d3d34ae11887e86d2e8fd03360
Jhonata Escatolini <jescatolini@gmail.com>
2023-06-19 07:26:26 -0300

    feat: errors handless

---
+++ src/services/meal/MealCreateService.spec.js
@@ -0,0 +1,45 @@
+const AppError = require("../../utils/AppError");
+require("dotenv/config");
+
+const MealRepositoryInMemory = require("../../repositories/meal/MealRepositoryInMemory");
+const MealCreateService = require("./MealCreateService");
+
+describe("MealCreateService", () => {
+  let mealRepositoryInMemory;
+  let mealCreateService;
+
+  beforeEach(() => {
+    mealRepositoryInMemory = new MealRepositoryInMemory();
+    mealCreateService = new MealCreateService(mealRepositoryInMemory);
+  });
+
+  it("must register a meal successfully", async () => {
+    const meal = {
+      title: "Macarrão",
+      description: "Um prato italiano",
+      category: "Principais",
+      price: "32.05",
+    };
+
+    const result = await mealCreateService.execute(meal);
+
+    expect(result).toHaveProperty("id");
+  });
+
+  it("should return a 'title already registered' error", async () => {
+    const meal = {
+      title: "Macarrão",
+      description: "Um prato italiano",
+      category: "Principais",
+      price: "32.05",
+    };
+
+    await mealCreateService.execute(meal);
+
+    const mealWithTheSameTitle = meal;
+
+    await expect(
+      mealCreateService.execute(mealWithTheSameTitle)
+    ).rejects.toEqual(new AppError("Título já em uso."));
+  });
+});
